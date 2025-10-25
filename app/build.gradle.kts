c5f49e0344483b02d6d414a1a57a4d8629b991e1
VarBus <marcelvarela@hotmail.com>
2025-10-25 16:54:24 -0500

    feat: add transition logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,4 +93,10 @@ dependencies {
 
     // Hilt Navigation
     implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
